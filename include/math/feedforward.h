213f7a03ff258f5f8ce2c5a3f33d450cf8b59d1d
Farhan Jamil <71662961+FarhanJ2@users.noreply.github.com>
2025-10-14 22:50:02 -0400

    add feedforward

---
+++ include/math/feedforward.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <constants.h>
+
+class feedforward {
+    private:
+        float kS; // Static gain
+        float kV; // Velocity gain
+        float kA; // Acceleration gain
+
+    public:
+        feedforward(float s, float v, float a)
+            : kS(s), kV(v), kA(a) {}
+
+        double compute(double velocity, double acceleration, double dt) {
+            return (kS * (velocity != 0 ? (velocity > 0 ? 1 : -1) : 0)) + (kV * velocity) + (kA * acceleration);
+        }
+
+        double compute(double velocity, double acceleration) {
+            float dt = constants::dt;
+            return (kS * (velocity != 0 ? (velocity > 0 ? 1 : -1) : 0)) + (kV * velocity) + (kA * acceleration);
+        }
+};
